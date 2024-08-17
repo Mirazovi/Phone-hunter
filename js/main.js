11625b44ddd3f0ccf9045a87f40a388355de886c
Mirazovi <mdmirazhossainovi@gmail.com>
2024-08-17 11:42:19 +0600

    last commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-const loaderData = async (id,isShowAll) => {
+const loaderData = async (id=13,isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${id}`);
     const data = await res.json();
     const phones = data.data;
-    console.log(phones);
     SetData(phones,isShowAll);
 }
 const SetData = (phones,isShowAll) => {
@@ -29,14 +28,30 @@ const SetData = (phones,isShowAll) => {
             <h1 class="text-2xl font-bold ">${phone.phone_name}</h1>
             <p class="text-xl ">${phone.slug}</p>
             <div class="justify-center flex items-center flex-grow">
-            <button class="bg-[#0D6EFD] py-2 px-4 rounded-lg border text-xl text-center text-white  my-2">Show Details</button> 
+            <button onclick="handleShowDetails('${phone.slug}')" class="bg-[#0D6EFD] py-2 px-4 rounded-lg border text-xl text-center text-white  my-2">Show Details</button> 
             </div>
         `
         ContainerSet.appendChild(divContainer);
       })
     ToggleLoadingSpinner(false);
 }
-
+// Show Details 
+const handleShowDetails = async (id) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phone = data.data;
+    ShowHandleDetails(phone)
+}
+const ShowHandleDetails  = (id) =>{
+    my_modal_5.showModal();
+    const phone = document.getElementById('phone-container');
+    phone.innerHTML = `
+    <h1>${id.name}</h1>
+    <img src=${id.image} alt="">
+    <h1>${id.mainFeatures.storage}</h1>
+    <p>${id.mainFeatures.displaySize}</p>
+    `
+}
 //  handleSearchInput 
 const handleSearch = (isShowAll) =>{
     ToggleLoadingSpinner(true);
@@ -59,3 +74,4 @@ const ToggleLoadingSpinner = (isLoading) =>{
 const handleShowMore = () =>{
         handleSearch(true);
 }
+loaderData();
